455b1065de220e32eeac5f9f951d066729584758
Rohit <ts.rohitsen@gmail.com>
2024-06-15 17:23:30 +0600

    backend deployment

--- Backend/dbConnect.js
+++ Backend/dbConnect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host:process.env.DB_HOST,
     // database: 'test',
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
