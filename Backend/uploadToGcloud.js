6131b30f3ca21df0dd1321d9b970dc3f9a246c57
Rohit Sen <112565753+TSRohit99@users.noreply.github.com>
2024-06-19 02:19:20 +0600

    v2.1 Slight UI Change

--- Backend/uploadToGcloud.js
+++ Backend/uploadToGcloud.js
@@ -14,7 +14,7 @@ const multerGoogleStorage = multer({
     storage: multer.memoryStorage(),
     limits: {
         fileSize: 10 * 1024 * 1024, // Maximum file size (5MB)
-    },
+    }
 });
 
 const uploadToGoogleCloud = async (files,x) => {
